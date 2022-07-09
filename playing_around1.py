ade77e150f1a4c83608d6244a42336d1a4b0cfcb
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-07-09 18:16:26 +0200

    Playing around with few realizations of single qubit.

--- playing_around1.py
+++ playing_around1.py
@@ -27,6 +27,44 @@ qobj = assemble(qc2)
 result = sim.run(qobj).result()
 counts = result.get_counts()
 plot_histogram(counts)
-plt.show()
 
-initial_state = [1/sqrt(2), 1j/sqrt(2)]  # Define state |q_0>
+initial_state3 = [1/sqrt(2), 1j/sqrt(2)]  # Define state |q_0>
+initial_state4 = [sqrt(3)/sqrt(4), 1j/sqrt(4)]  # Define state |q_0>
+initial_state5 = [sqrt(3)/sqrt(7), sqrt(4)/sqrt(7)]  # Define state |q_0>
+
+qc3 = QuantumCircuit(1) # Must redefine qc
+qc3.initialize(initial_state3, 0) # Initialize the 0th qubit in the state `initial_state`
+qc3.save_statevector() # Save statevector
+qobj = assemble(qc3)
+state = sim.run(qobj).result().get_statevector() # Execute the circuit
+print(state)           # Print the result
+
+qobj = assemble(qc3)
+results = sim.run(qobj).result().get_counts()
+plot_histogram(results)
+
+
+qc4 = QuantumCircuit(1) # Must redefine qc
+qc4.initialize(initial_state4, 0) # Initialize the 0th qubit in the state `initial_state`
+qc4.save_statevector() # Save statevector
+qobj = assemble(qc4)
+state = sim.run(qobj).result().get_statevector() # Execute the circuit
+print(state)           # Print the result
+
+qobj = assemble(qc4)
+results = sim.run(qobj).result().get_counts()
+plot_histogram(results)
+
+
+qc5 = QuantumCircuit(1) # Must redefine qc
+qc5.initialize(initial_state5, 0) # Initialize the 0th qubit in the state `initial_state`
+qc5.save_statevector() # Save statevector
+qobj = assemble(qc5)
+state = sim.run(qobj).result().get_statevector() # Execute the circuit
+print(state)           # Print the result
+
+qobj = assemble(qc5)
+results = sim.run(qobj).result().get_counts()
+plot_histogram(results)
+
+plt.show()
