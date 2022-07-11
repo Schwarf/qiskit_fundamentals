00a3b08ad6a570acb8a93c157d564f41247576c8
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-07-11 16:14:15 +0200

    Phase kickback

--- multiple_qubits_gates/GatesAndKroneckerProduct.m
+++ multiple_qubits_gates/GatesAndKroneckerProduct.m
@@ -1,9 +1,10 @@
-Xgate ={{0,1}, {1,0}}
-Ygate = {{0, -I}, {I, 0}}
-Zgate = {{1,0}, {0,-1}}
-Pgate[phi_] := {{1, 0}, {0,Exp[I*phi]}}
-Hgate = 1/Sqrt[2]*{{1,1}, {1,-1}}
-Sgate = Pgate[Pi/2]
-Tgate = Pgate[Pi/4]
-KroneckerProduct[Xgate, KroneckerProduct[Zgate, Hgate]]
+XGate = {{0, 1}, {1, 0}}
+YGate = {{0, -I}, {I, 0}}
+ZGate = {{1, 0}, {0, -1}}
+PGate[phi_] := {{1, 0}, {0, Exp[I * phi]}}
+HGate = 1 / Sqrt[2] * {{1, 1}, {1, -1}}
+SGate = PGate[Pi / 2]
+TGate = PGate[Pi / 4]
+CnotGate = {{1, 0, 0, 0}, {0, 0, 0, 1}, {0, 0, 1, 0}, {0, 1, 0, 0}}
+KroneckerProduct[XGate, KroneckerProduct[ZGate, HGate]]
 
