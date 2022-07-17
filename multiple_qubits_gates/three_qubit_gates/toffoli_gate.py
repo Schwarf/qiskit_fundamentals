84dd660f3957498ae63949973a105e2f3880686d
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-07-17 21:23:29 +0200

    Controlled u gate strange ...

--- multiple_qubits_gates/three_qubit_gates/toffoli_gate.py
+++ multiple_qubits_gates/three_qubit_gates/toffoli_gate.py
@@ -1,7 +1,20 @@
 import matplotlib.pyplot as plt
+from math import pi
 from qiskit import QuantumCircuit, QuantumRegister, assemble, Aer, transpile
+import numpy as np
 
 backend = Aer.get_backend('unitary_simulator')
+def controlled_u_gate_two_qubit_gates(qubits: QuantumRegister, circuit: QuantumCircuit, theta: float) -> QuantumCircuit:
+    target = qubits[2]
+    control1 = qubits[0]
+    control2 = qubits[1]
+    circuit.cp(theta, control2, target)
+    circuit.cx(control1, control2)
+    circuit.cp(-theta, control2, target)
+    circuit.cx(control1, control2)
+    circuit.cp(theta, control1, target)
+    circuit.draw()
+    return circuit
 
 qubits = QuantumRegister(3)
 
@@ -9,7 +22,21 @@ qc = QuantumCircuit(qubits)
 qc.ccx(qubits[0], qubits[1], qubits[2])
 qc.draw()
     
+qubits2 = QuantumRegister(3)
+qc2 = QuantumCircuit(qubits2)
+controlled_u_gate_two_qubit_gates(qubits2, qc2, np.pi)
+
+
+
 job = backend.run(transpile(qc, backend))
-result=job.result().get_unitary(qc, decimals=3)
+result=job.result().get_unitary(qc)
 print(result)
+
+
+job2 = backend.run(transpile(qc2, backend))
+result2=job2.result().get_unitary(qc2)
+print(result2)
+
 plt.show()
+
+
