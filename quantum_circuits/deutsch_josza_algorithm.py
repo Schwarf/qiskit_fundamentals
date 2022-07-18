a1b01936a3108e7c3c140ae70bc8511da7cb704f
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-07-18 15:06:46 +0200

    Implement method for Deutsch-Josza circuit.

--- quantum_circuits/deutsch_josza_algorithm.py
+++ quantum_circuits/deutsch_josza_algorithm.py
@@ -1,14 +1,38 @@
 # initialization
 import numpy as np
 from matplotlib import pyplot as plt
-
 # importing Qiskit
 from qiskit import IBMQ, Aer
 from qiskit.providers.ibmq import least_busy
 from qiskit import QuantumCircuit, assemble, transpile
+from qiskit.circuit import Gate
 
 # import basic plot tools
 from qiskit.visualization import plot_histogram
+
+
+def deutsch_josza_circuit(oracle_gate: Gate, number_of_inputs: int) -> QuantumCircuit:
+    number_of_bits = number_of_inputs
+    number_of_qubits = number_of_inputs + 1
+    dj_circuit = QuantumCircuit(number_of_qubits, number_of_bits)
+    # all qubits initialize to |0> ... last one shall be initialized to |1> ...0-indexed
+    dj_circuit.x(number_of_inputs)
+    # Apply Hadamard to all input-qubits and the last one
+    for qubit in range(number_of_qubits):
+        dj_circuit.h(qubit)
+    # Apply oracle function: Note this function is applied to all qubits, although is has only an effect on the last
+    # qubit
+    dj_circuit.append(oracle_gate, range(number_of_qubits))
+    # Apply Hadamard to input qubits, the last qubit can now be ignored (in the next step measurement only applied to
+    # input)
+    for qubit in range(number_of_inputs):
+        dj_circuit.h(qubit)
+
+    for classic_bit in range(number_of_bits):
+        dj_circuit.measure(classic_bit)
+
+    return dj_circuit
+
 
 
 input_size = 3
