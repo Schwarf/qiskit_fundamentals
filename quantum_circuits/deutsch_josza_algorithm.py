50b40a236aecb1edc4e849f95a0cae55596afed5
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-07-18 14:44:33 +0200

    Deustch-Josza algorithm. Concept understood, but detailed documentation is needed.

---
+++ quantum_circuits/deutsch_josza_algorithm.py
@@ -0,0 +1,71 @@
+# initialization
+import numpy as np
+from matplotlib import pyplot as plt
+
+# importing Qiskit
+from qiskit import IBMQ, Aer
+from qiskit.providers.ibmq import least_busy
+from qiskit import QuantumCircuit, assemble, transpile
+
+# import basic plot tools
+from qiskit.visualization import plot_histogram
+
+
+input_size = 3
+const_oracle = QuantumCircuit(input_size + 1)
+
+output = np.random.randint(2)
+if output == 1:
+    const_oracle.x(input_size)
+const_oracle.draw()
+
+balanced_oracle = QuantumCircuit(input_size+1)
+b_str = "101"
+
+# Place X-gates
+for qubit in range(len(b_str)):
+    if b_str[qubit] == '1':
+        balanced_oracle.x(qubit)
+
+# Use barrier as divider
+balanced_oracle.barrier()
+
+# Controlled-NOT gates
+for qubit in range(input_size):
+    balanced_oracle.cx(qubit, input_size)
+
+balanced_oracle.barrier()
+# Place X-gates
+for qubit in range(len(b_str)):
+    if b_str[qubit] == '1':
+        balanced_oracle.x(qubit)
+
+# Show oracle
+balanced_oracle.draw()
+
+dj_circuit = QuantumCircuit(input_size+1, input_size)
+
+# Apply H-gates
+for qubit in range(input_size):
+    dj_circuit.h(qubit)
+
+# Put qubit in state |->
+dj_circuit.x(input_size)
+dj_circuit.h(input_size)
+
+# Add oracle
+dj_circuit += balanced_oracle
+
+# Repeat H-gates
+for qubit in range(input_size):
+    dj_circuit.h(qubit)
+dj_circuit.barrier()
+
+# Measure
+for i in range(input_size):
+    dj_circuit.measure(i, i)
+
+dj_circuit.draw()
+
+plt.show()
+
