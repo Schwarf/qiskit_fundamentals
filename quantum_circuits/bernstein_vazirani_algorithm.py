af105ebf2a49a039f19c45ce7825cb0335c45243
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-07-19 16:54:37 +0200

    Run Bernstein-Vazirani algorithm on real device.

--- quantum_circuits/bernstein_vazirani_algorithm.py
+++ quantum_circuits/bernstein_vazirani_algorithm.py
@@ -1,10 +1,11 @@
 import numpy as np
 from matplotlib import pyplot as plt
-from qiskit import Aer
+from qiskit import Aer, IBMQ
 from qiskit import QuantumCircuit, assemble, transpile
 from qiskit.circuit import Gate
+from qiskit.providers.ibmq import least_busy
 from qiskit.visualization import plot_histogram
-
+from qiskit.tools.monitor import job_monitor
 
 def bernstein_vazirani_circuit(oracle_gate: Gate, number_of_inputs: int) -> QuantumCircuit:
     number_of_bits = number_of_inputs
@@ -48,22 +49,47 @@ def construct_oracle(hidden_string: str) -> Gate:
     oracle_gate.name = "Bernstein-Vazirani-Oracle"
     return oracle_gate
 
-number_of_inputs = 5
-random_binary_string_number = np.random.randint(1, 2**number_of_inputs)
-random_binary_string = format(random_binary_string_number, '0'+str(number_of_inputs)+'b')
+
+number_of_inputs = 4
+random_binary_string_number = np.random.randint(1, 2 ** number_of_inputs)
+random_binary_string = format(random_binary_string_number, '0' + str(number_of_inputs) + 'b')
+print(random_binary_string)
+#random_binary_string = "11101101"
 
 oracle_gate = construct_oracle(hidden_string=random_binary_string)
 bv_circuit = bernstein_vazirani_circuit(oracle_gate=oracle_gate, number_of_inputs=number_of_inputs)
 
 aer_sim = Aer.get_backend('aer_simulator')
 transpiled_bv_circuit = transpile(bv_circuit, aer_sim)
+"""
 shots = 1024
 qobj = assemble(transpiled_bv_circuit)
 results = aer_sim.run(qobj).result()
 answer = results.get_counts()
 
 plot_histogram(answer)
-print(random_binary_string)
+
 plt.show()
 
+"""
+################################
+# Run on a realistic device
+################################
 
+IBMQ.load_account()
+provider = IBMQ.get_provider(hub='ibm-q')
+provider.backends()
+backend = least_busy(provider.backends(filters=lambda x: x.configuration().n_qubits <6 and x.configuration().n_qubits > 1 and
+                                       not x.configuration().simulator and x.status().operational==True))
+
+print("least busy backend: ", backend)
+shots = 1024
+transpiled_bv_circuit = transpile(transpiled_bv_circuit, backend)
+job = backend.run(transpiled_bv_circuit, shots=shots)
+
+job_monitor(job, interval=2)
+results = job.result()
+answer = results.get_counts()
+plot_histogram(answer)
+
+plt.show()
