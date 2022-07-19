99f5fa58ed6a30a826b1aab7b904f1b940d810ad
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-07-19 12:45:19 +0200

    Start refactoring of DJ oracle.

--- quantum_circuits/deutsch_josza_algorithm.py
+++ quantum_circuits/deutsch_josza_algorithm.py
@@ -1,15 +1,60 @@
-# initialization
 import numpy as np
 from matplotlib import pyplot as plt
-# importing Qiskit
 from qiskit import IBMQ, Aer
 from qiskit.providers.ibmq import least_busy
 from qiskit import QuantumCircuit, assemble, transpile
 from qiskit.circuit import Gate
-
-# import basic plot tools
 from qiskit.visualization import plot_histogram
 
+
+
+def _constant_oracle(oracle_qc: QuantumCircuit, number_of_inputs: int) -> QuantumCircuit:
+    # For a constant oracle we just have to choose 0 or 1 and set the out-put qubit accordingly
+    output_value = np.random.randint(2)
+    # If output_value is one, we set the output-qubit (the last one) to |1>
+    if output_value == 1:
+        oracle_qc.x(number_of_inputs)
+    return oracle_qc
+
+def _balanced_oracle(oracle_qc: QuantumCircuit, number_of_inputs: int) -> QuantumCircuit:
+    # For a balanced oracle we use CNOT gates that take on one-inout qubit as control and target the output qubit
+    # To randomize we randomly wrap (befor and after CNOT) input-qubits with X-gates.
+
+    # Generate random number between 1 and 2**n-1 (n =number__of_inputs) ... the corresponding binary string
+    # tells us the positions of the qubits that will be wrapped in X-gates
+    random_binary_string_number = np.random.randint(1, 2 ** number_of_inputs)
+    random_binary_string = format(random_binary_string_number, '0' + str(number_of_inputs) + 'b')
+    # Now place first X-gates
+    for qubit_position in range(len(random_binary_string)):
+        if random_binary_string[qubit_position] == 1:
+            oracle_qc.x(qubit_position)
+
+    # Now we apply the CNOT gates to all qubits
+    for qubit_position in range(number_of_inputs):
+        oracle_qc.cx(qubit_position)
+    # Now place the final X-gates
+    for qubit_position in range(len(random_binary_string)):
+        if random_binary_string[qubit_position] == 1:
+            oracle_qc.x(qubit_position)
+
+    return oracle_qc
+
+def construct_oracle(number_of_inputs: int, is_constant_oracle: False) -> Gate:
+    # There is one output-qubit (the last one) which is either 1 or 0.
+    number_of_outputs = 1
+    number_of_qubits = number_of_inputs + number_of_outputs
+
+    oracle_qc = QuantumCircuit(number_of_qubits)
+
+    if is_constant_oracle:
+        qc_oracle = _constant_oracle(oracle_qc, number_of_inputs)
+    if not is_constant_oracle:
+        qc_oracle = _balanced_oracle(oracle_qc, number_of_inputs)
+
+    # Create the gate
+    oracle_gate = oracle_qc.to_gate()
+    oracle_gate.name = "Oracle"
+    return oracle_gate
 
 def deutsch_josza_circuit(oracle_gate: Gate, number_of_inputs: int) -> QuantumCircuit:
     number_of_bits = number_of_inputs
@@ -32,6 +77,7 @@ def deutsch_josza_circuit(oracle_gate: Gate, number_of_inputs: int) -> QuantumCi
         dj_circuit.measure(classic_bit)
 
     return dj_circuit
+
 
 
 
